ab4f3606cd579236d56c1a7da22493a3432d24f6
Rainy <Rainy@zhaosijingdembp.lan>
2016-07-04 11:30:25 +0800

    dd

--- src/android/SaveToPhotoAlbum.java
+++ src/android/SaveToPhotoAlbum.java
@@ -337,8 +337,8 @@ public class SaveToPhotoAlbum extends CordovaPlugin {
 			}
 
 			// now we create the image in the folder
-			File imageFile = new File(folder, fileName);
-			FileOutputStream out = new FileOutputStream(imageFile);
+			//File imageFile = new File(folder, fileName);
+			FileOutputStream out = new FileOutputStream(folder+"/"+fileName);
 			// compress it
 			bmp.compress(compressFormat, quality, out);
 			out.flush();
