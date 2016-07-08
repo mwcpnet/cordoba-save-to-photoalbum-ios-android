af56a904282bae880c65465effed6be55da7ae42
Rainy <Rainy@zhaosijingdembp.lan>
2016-07-08 13:43:36 +0800

    String url = MediaStore.Images.Media.insertImage(cordova.getActivity().getContentResolver(), mBitmap, filename, filename); 					if(!mBitmap.isRecycled() ){ 						mBitmap.recycle();   //回收图片所占的内存 //				         system.gc()  //提醒系统及时回收 					}

--- src/android/SaveToPhotoAlbum.java
+++ src/android/SaveToPhotoAlbum.java
@@ -67,7 +67,7 @@ public class SaveToPhotoAlbum extends CordovaPlugin {
 					getImage(imgname, filename);
 					
 					String url = MediaStore.Images.Media.insertImage(cordova.getActivity().getContentResolver(), mBitmap, filename, filename);
-					if(!mBitmap.isRecycle() ){ 
+					if(!mBitmap.isRecycled() ){ 
 						mBitmap.recycle();   //回收图片所占的内存 
 //				         system.gc()  //提醒系统及时回收 
 					} 
